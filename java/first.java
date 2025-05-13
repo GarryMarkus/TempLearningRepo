f2e445ff5a216a08b6d1476064d56a115622fd19
GarryMarkus <premsinghiitjee@gmail.com>
2025-05-14 03:07:04 +0530

    started

--- java/first.java
+++ java/first.java
@@ -1,10 +1,8 @@
-// Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 /**
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-import java.lang.*;
 class first {
     public static void main(String[] args) {
         System.out.println("Hello World");   
