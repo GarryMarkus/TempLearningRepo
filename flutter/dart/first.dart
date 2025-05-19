43c1a9414196a73e738852be410091d3a4041e40
GarryMarkus <premsinghiitjee@gmail.com>
2025-05-19 07:28:41 +0530

    dart continued

--- flutter/dart/first.dart
+++ flutter/dart/first.dart
@@ -1,3 +1,11 @@
-void main(){
-  print ("Hello world");
-}
+main() { // the entrypoint of an Dart app
+    var a = 'world, good morning'; // declaring and initializing variable
+    print('hello $a'); // call function to print to display output
+ }
+
+// this is a single line comment
+
+ /*
+ This is a long multiline comment
+ */
+    
