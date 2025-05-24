91cbec9d86aeba49ab2160b30c5038dc484d6773
GarryMarkus <premsinghiitjee@gmail.com>
2025-05-24 17:52:11 +0530

    daily commits

--- python/practiceCodes/tcs1.py
+++ python/practiceCodes/tcs1.py
@@ -1,12 +1,1 @@
-n=int(input())
-strings=input().split()
-s=input()
-count=0
-for str in strings:
-    for ch in str:
-        result=s.replace(ch,"")
-        s=result
-        if(s==""): break
-    count+=1
-print(s) 
-print(count)
+print("hello")
