e07f2386d50bc62737e38a39c6ec57c18b0c9280
GarryMarkus <premsinghiitjee@gmail.com>
2025-05-30 18:35:57 +0530

    javascript learned

---
+++ javascript/01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"user strict"; //treat all JS code as newer version
+
+let variableAss = "ass"
+
+//alert("pop up message of browser") //when used as a binding through an html file gives up a pop-up message in the browser, on terminal it will give error
+
+/* datatypes
+number
+bigint
+string
+boolean
+null => standalone value
+undefined
+symbol => unique
+*/
+
+console.log(typeof variableAss)
+
+console.log(typeof null)
+
+console.log(typeof undefined)
