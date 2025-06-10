7437d2faf4fe56ef40f37fc4cdeafe779c3b84f2
GarryMarkus <premsinghiitjee@gmail.com>
2025-06-10 17:22:25 +0530

    java started

---
+++ java/abdulBariClasses/Variables.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class Variables{
+    public static void main(String arg[]){
+        byte b; //initial value is not assigned, variable was only declared
+        b=9; //variable was assigned a value, declared + assigned = initialised 
+        System.out.println(b);
+
+    }
+}
