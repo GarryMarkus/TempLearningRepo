94aabb00ecee202f4a31f00cea331ee442f30e2f
GarryMarkus <premsinghiitjee@gmail.com>
2025-06-13 04:00:03 +0530

    java data restructured

--- java/dataSizeRange.java
+++
@@ -1,7 +0,0 @@
-class dataSizeRange {
-    public static void main(String args[]){
-        System.out.println("Integer min: " + Integer.MIN_VALUE);
-        System.out.println("Integer max: " + Integer.MAX_VALUE);
-        System.out.println("Integer size: " + Integer.BYTES);
-    }
-}
