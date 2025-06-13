0ebf25ec9da1f98cd847d1acc2962ba42c42e3bb
GarryMarkus <premsinghiitjee@gmail.com>
2025-06-13 06:20:36 +0530

    myfirst.java done

---
+++ java/abdulBariLec/1. Introduction/MyFirst.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+
+class MyFirst{
+    public static void main(String arg[]){
+        System.out.println("Hello World");
+    }
+}
+
+//try running it using command line
+//javac MyFirst.java
+//java MyFirst
+
+//now in the main method, add below line
+//System.out.println(arg[0])
+//not run using command line
+//javac MyFirst.java
+//java MyFirst hello
