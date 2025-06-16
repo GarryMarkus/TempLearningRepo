a7bc41e470bbd530cb65bc9705339f5ec0ee6fca
GarryMarkus <premsinghiitjee@gmail.com>
2025-06-17 05:14:28 +0530

    17/06/25

---
+++ java/abdulBariLec/2. Data Types - Variables and Literals/dataSizeRange.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class dataSizeRange {
+    public static void main(String args[]){
+        System.out.println("Integer min value: " + Integer.MIN_VALUE);
+        System.out.println("Integer max value: " + Integer.MAX_VALUE);
+        System.out.println("Integer size in byte: " + Integer.BYTES);
+    }
+}
