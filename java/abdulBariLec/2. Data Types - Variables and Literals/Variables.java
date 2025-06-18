04407f5bcce0f8321d5278e78035c2e0927b6c25
GarryMarkus <premsinghiitjee@gmail.com>
2025-06-19 00:45:23 +0530

    19/06/25 done

---
+++ java/abdulBariLec/2. Data Types - Variables and Literals/Variables.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+class Variables {
+    public static void main(String args[]){
+        byte b=4;
+        short s=100;
+        int i=1000;
+        float f=1.23f;//to assign a float value add "f" in the end of the value
+        char c='A';
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(c);
+
+    } 
+}
