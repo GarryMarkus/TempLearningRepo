1ab278284d8209f2fa687407a618ca6a7110b930
GarryMarkus <premsinghiitjee@gmail.com>
2025-07-08 17:23:13 +0530

    javascript escape sequences

--- javascript/4escapingLiteralQuotes.js
+++ javascript/4escapingLiteralQuotes.js
@@ -1,2 +1,14 @@
 var myStr = "this is my string containing \"double quotes\" hehehehe"
 console.log(myStr)
+
+/* 
+Escape Characters:
+\' single quotes
+\" double quotes
+\\ backslash
+\n new line
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
