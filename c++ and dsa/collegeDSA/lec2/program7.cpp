59b45ff88baba4a13f0beb9493ce8cf9a7355569
GarryMarkus <premsinghiitjee@gmail.com>
2025-08-26 11:24:03 +0530

    college dsa training day 2

---
+++ c++ and dsa/collegeDSA/lec2/program7.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n, total;
+        string s;
+        cin >> n >> s;
+        if(s=="INT"){
+            int arr[n];
+            for(int i=0; i<n; i++){
+                cin >> arr[i];
+            }
+            total = accumulate(arr, arr + n, 0);
+            cout << total << endl;
+        }
+        else{
+            int wMaxLength=0;
+            string word;
+            for(int i=0; i<n; i++){
+                cin >> word;
+                if(word.size()>wMaxLength){
+                    wMaxLength = word.size();
+                
+                }
+            }
+            cout << wMaxLength << endl;
+        }
+    }
+}
