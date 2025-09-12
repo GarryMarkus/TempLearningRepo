9f0d0527a1f1f42d9b8351e13f459ebb6f344fe4
GarryMarkus <premsinghiitjee@gmail.com>
2025-09-12 12:51:08 +0530

    progress

--- c++ and dsa/dsa/myCode/Step 4/intro.cpp
+++ c++ and dsa/dsa/myCode/Step 4/intro.cpp
@@ -3,16 +3,63 @@ using namespace std;
 
 class Hero{
 
-    //properties
-    int health;
+    //getter and setter for private properties
+    private:
+    int level;
+
+    public: 
+    int getLevel(){
+        return level;
+    }
+
+    int setLevel(int l){
+        level = l;
+    }
+
+    public:
+
+    //a constructor names as h1.Hero() will get created and called automatically, if we create it ourself then it will execute the constructor ceated by us
+
+    Hero(){
+        cout << endl << endl << endl << endl << "constructor cerated" << endl << endl << endl << endl << endl;
+    }
+
+
+    //properties, public by default
+    int health = 9;
 };
 
 int main(){
 
-    //object creation
+    //object creation: statically
     Hero h1;
 
     cout << "size of h1 is " << sizeof(h1) << endl;
+
+    int l1;
+    cout << "enter desired level value ";
+    cin >> l1;
+
+    h1.setLevel(l1);
+    cout << "level of h1 is " << h1.getLevel() << endl;
+
+
+    //dyanmically creating an object
+    Hero *h2 = new Hero;
+    cout << "size of h2 is " << sizeof((*h2)) << endl;
+    
+
+    int l2;
+    cout << "health of h2 is " << h2->health << endl;
+
+    cout << "enter desired level value ";
+    cin >> l2;    
+
+    (*h2).setLevel(l2);
+    cout << "level of h2 is " << (*h2).getLevel() << endl;
+    cout << "or " << h2->getLevel();
+
+    
 
     return 0;
 }
