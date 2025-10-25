1901a447b4f5ca69922319950302b60f058f3086
GarryMarkus <premsinghiitjee@gmail.com>
2025-10-26 02:08:04 +0530

    cleaning

--- c++ and dsa/collegeDSA/lec1/program4.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    float div;
-    cin >> div;
-
-    cout << div/2;
-}
--- c++ and dsa/redundantData/charFunction.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    string name = "Frank";
-    if(name.find("g")<name.size()) cout << "yes";
-    else cout << "No";
-    return 0;
-}
